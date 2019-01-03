ef49295277b5ae9b454a36b6467792db93e542cb
tmiquel <45764690+tmiquel@users.noreply.github.com>
2019-01-03 01:21:18 +0100

    Update main.js
    Remove space in description

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ BetterMotherFuckingWebsite cette popup.
 setTimeout(myPopUp,10000)
 function myPopUp() {
 
-	if (confirm("Hey le site xxxvidsxxx est trop bien. \
+	if (confirm("Hey le site xxxvidsxxx est trop bien.
 	Viens dessus stp please")) {
 	window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 	}
