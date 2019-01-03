48e6fd8f72d6da4cc11689c18c29a3e4c4fbb4e8
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-01-03 01:14:34 +0100

    Initial setup - .html, .css and .js files added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tmiquel.github.io
-Training on js with THP
+Training on js with THP.
+A BetterMotherFuckingWebsite with a PopUp Window that redirects to "Rick Astley - Never Gonna Give You Up (Video)" on YouTube.
+Credentials to http://bettermotherfuckingwebsite.com/
+
+
---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+/* tu dois afficher une popup en plein milieu 
+de la page qui affiche "Hey le site xxxvidsxxx est 
+trop bien. Viens dessus stp please" au bout de 10 
+secondes sur le site. La popup peut être en alert().
+
+Ta mission est d'implémenter sur le site 
+BetterMotherFuckingWebsite cette popup.
+*/
+
+setTimeout(myPopUp,10000)
+function myPopUp() {
+
+	if (confirm("Hey le site xxxvidsxxx est trop bien. \
+	Viens dessus stp please")) {
+	window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+	}
+
+	}
+
+
+
+
